c78beb7fce2604ca727a13d7886de4bb2d6562ee
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-21 21:13:48 +0700

    cong nhan chia promise

--- d.js
+++ d.js
@@ -11,6 +11,29 @@ function cong(a, b) {
     });
 }
 
+function nhan(a, b) {
+    return new Promise((resolve, reject) => {
+        const url = `http://async1704.herokuapp.com/tinh/NHAN/${a}/${b}`;
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error'));
+        request(url, (error, response, body) => {
+            if (error) return reject(error);
+            resolve(body);
+        });
+    });
+}
+
+function chia(a, b) {
+    return new Promise((resolve, reject) => {
+        const url = `http://async1704.herokuapp.com/tinh/CHIA/${a}/${b}`;
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error'));
+        request(url, (error, response, body) => {
+            if (error) return reject(error);
+            resolve(body);
+        });
+    });
+}
+
 cong(4, 5)
-.then(result => console.log(result))
-.catch(error => console.log(error.message));
+.then(tong => nhan(tong, 6))
+.then(tich => chia(tich, 2))
+.then(kq => console.log(kq));
