06b6cb35a19a6cb4bd4666a9ba5ec579ac2cca45
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-21 20:55:36 +0700

    wrap to function

--- c.js
+++ c.js
@@ -9,7 +9,38 @@ function cong(a, b, cb) {
     });
 }
 
-cong('x', 5, (error, total) => {
-    if (error) return console.log(error.message);
-    console.log(total);
+function nhan(a, b, cb) {
+    const url = `http://async1704.herokuapp.com/tinh/NHAN/${a}/${b}`;
+    if (isNaN(a) || isNaN(b)) return cb(new Error('Type error'));
+    request(url, (error, response, body) => {
+        if (error) return cb(error);
+        cb(null, body);
+    });
+}
+
+function chia(a, b, cb) {
+    const url = `http://async1704.herokuapp.com/tinh/CHIA/${a}/${b}`;
+    if (isNaN(a) || isNaN(b)) return cb(new Error('Type error'));
+    request(url, (error, response, body) => {
+        if (error) return cb(error);
+        cb(null, body);
+    });
+}
+
+function tinhDienTichHinhThang(a, b, h, cb) {
+    cong(a, b, (errorTong, tong) => {
+        if (errorTong) return cb(errorTong);
+        nhan(tong, h, (errorTich, tich) => {
+            if (errorTich) return cb(errorTich);
+            chia(tich, 2, (error, kq) => {
+                if (error) return cb(error);
+                cb(null, kq);
+            });
+        });
+    });
+}
+
+tinhDienTichHinhThang(4, 5, 6, (error, kq) => {
+    if (error) return console.log(error);
+    console.log(kq);
 });
