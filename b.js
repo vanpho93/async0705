0805fac3c19fadcf11dc3f6c6136a852b9becd00
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-21 19:52:44 +0700

    add catch 404 error

--- b.js
+++ b.js
@@ -1,8 +1,9 @@
 const request = require('request');
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=SaiGon'
+const url = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=11111111';
 
 request(url, { json: true }, (error, response, body) => {
     if (error) return console.log(error);
+    if (!body.main) return console.log(body.message);
     console.log(body.main.temp);
 });
